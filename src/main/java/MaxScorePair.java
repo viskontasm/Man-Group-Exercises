fe0e06430f92b1347b87f36333bdee50f5677ba3
Marius Viskontas <viskontasm@gmail.com>
2021-06-28 13:11:30 +0300

    MaxScorePair created

---
+++ src/main/java/MaxScorePair.java
@@ -0,0 +1,25 @@
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.List;
+
+public class MaxScorePair {
+
+    public static Pair<String, String> getPair(List<String> words) {
+        if (words == null || words.size() < 2) {
+            return null;
+        }
+        int maxScore = -1;
+        Pair<String, String> pair = null;
+        for (int i = 0; i < words.size() - 1; i++) {
+            for (int j = i +1 ; j < words.size(); j++) {
+                int currentScore = ScoreStrings.getScore(words.get(i), words.get(j));
+                if (maxScore < currentScore) {
+                    pair = ImmutablePair.of(words.get(i), words.get(j));
+                    maxScore = currentScore;
+                }
+            }
+        }
+        return pair;
+    }
+}
---
+++ src/test/java/MaxScorePairTest.java
@@ -0,0 +1,49 @@
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxScorePairTest {
+
+    @Test
+    public void testPairWhenListNull() {
+        assertNull(MaxScorePair.getPair(null));
+    }
+
+    @Test
+    public void testPairWhenListIsEmpty() {
+        assertNull(MaxScorePair.getPair(new ArrayList<>()));
+    }
+
+    @Test
+    public void testPairWhenListSizeBellowTwo() {
+        assertNull(MaxScorePair.getPair(new ArrayList<>() {{ add("1111"); }}));
+    }
+
+    @Test
+    public void testPairWhenAllItemsShared() {
+        Pair<String, String> result = MaxScorePair.getPair(new ArrayList<>() {{ add("a"); add("ab");
+            add("acc"); add("addd"); add("abcdefg"); }});
+        Pair<String, String> expected = ImmutablePair.of("a", "ab" );
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testPairWhenCharacterShared() {
+        Pair<String, String> result = MaxScorePair.getPair(new ArrayList<>() {{ add("a"); add("bb");
+            add("ccc"); add("bddd"); add("abcdefg"); }});
+        Pair<String, String> expected = ImmutablePair.of("ccc", "bddd" );
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testScoreWhenCharacterNotShared() {
+        Pair<String, String> result = MaxScorePair.getPair(new ArrayList<>() {{ add("1111"); add("zzzz");
+            add("aaaaaa"); add("ddd"); }});
+        Pair<String, String> expected = ImmutablePair.of("1111", "aaaaaa" );
+        assertEquals(expected, result);
+    }
+}
